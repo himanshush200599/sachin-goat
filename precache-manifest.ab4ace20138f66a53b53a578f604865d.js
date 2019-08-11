7bd07b15814c676e21ab8ad0105ebc9b6b9c551c
himanshush200599 <himanshush214@gmail.com>
2019-08-11 21:35:11 +0530

    Updates

---
+++ precache-manifest.ab4ace20138f66a53b53a578f604865d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b6a977ddeb0f7f477c3cdc123076d1",
+    "url": "/sachin-goat/index.html"
+  },
+  {
+    "revision": "31a8135f88bd05b9f987",
+    "url": "/sachin-goat/static/css/main.06f5083c.chunk.css"
+  },
+  {
+    "revision": "177f421eb590a77ee8d5",
+    "url": "/sachin-goat/static/js/2.82341acd.chunk.js"
+  },
+  {
+    "revision": "31a8135f88bd05b9f987",
+    "url": "/sachin-goat/static/js/main.bb5aefa3.chunk.js"
+  },
+  {
+    "revision": "94a8eed3cbc11ca25b43",
+    "url": "/sachin-goat/static/js/runtime~main.760252dc.js"
+  }
+]);
